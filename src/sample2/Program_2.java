c1a7c92b8ca9c459fbc6f78ef9577ca0f6ca1c95
loganayaki <logubscmaths96@gmail.com>
2022-02-20 18:16:32 +0530

    changed again

--- src/sample2/Program_2.java
+++ src/sample2/Program_2.java
@@ -4,7 +4,7 @@ public class Program_2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Loga2");
+System.out.println("Hello Loganayaki ");
 	}
 
 }
